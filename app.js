7e47cee9e23e9785148e9c4fe7a501c9f3151fa0
Carsten Ringe <carsten@kopis.de>
2013-05-07 22:30:42 +0200

    Recreate express app. Add highcharts.

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
+
+/**
+ * Module dependencies.
+ */
+
 var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
   , mongo = require('mongodb');
 
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb'; 
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 
 var app = express();
 
-app.configure(function() {
-  app.use(express.logger());
-  app.use(express.bodyParser());
-  app.use(app.router);
-});
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
 app.get('/', function(req, res) {
   mongo.Db.connect(mongoUri, function (err, db) {
     db.collection('imp', function(er, collection) {
       collection.find().toArray(function(er,rs) {
          res.header('Content-Type', 'application/json');
-         res.send(rs);
+         if(req.param('cb')) {
+           res.send(req.param('cb') + '(' + JSON.stringify(rs) + ')');
+         } else {
+           res.send(rs);
+         }
       });
     });
   });
@@ -31,9 +55,21 @@ app.post('/', function(req, res) {
     });
   });
 });
+app.get('/graph', function(req, res) {
+mongo.Db.connect(mongoUri, function (err, db) {
+    db.collection('imp', function(er, collection) {
+      collection.find().toArray(function(er,rs) {
+         var timeseries = new Array();
+         for(i in rs) {
+           timeseries.push([rs[i].time, rs[i].payload.value]);
+         }
+         res.header('Content-Type', 'text/html');
+         res.render('graph', {title: 'Graph', data: JSON.stringify(timeseries)});
+      });
+    });
+  });
+  });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log('Listening on ' + port);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
